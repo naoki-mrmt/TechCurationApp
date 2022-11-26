58f5502151494310123e1696212d3b2fb3e14dd0
naoki-mrmt <20cm0135@jec.ac.jp>
2022-11-26 16:49:03 +0900

    modification of directory structure

---
+++ TechCurationApp/Screen/LaunchScreen/LaunchScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreenView.swift
+//  TechCurationApp
+//
+//  Created by Naoki Muramoto on 2022/11/26.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+    }
+}
